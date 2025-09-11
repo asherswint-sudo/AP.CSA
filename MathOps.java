18f6a9862f1d607eaac7234ce03870dcb2866842
asherswint-sudo <asherswint@billericak12.com>
2025-09-11 09:50:21 -0400

    f

---
+++ MathOps.java
@@ -0,0 +1,13 @@
+public class MathOps{
+    public static void main (String[] args) {
+        int x = 5;
+        int y = 8;
+        double m = 6.25;
+        System.out.println(x + " " + y + " " + m);
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println((double)x/y);
+        System.out.println("x+y is " + x + y);
+    }
+}
