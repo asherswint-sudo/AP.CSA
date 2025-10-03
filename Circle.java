93bec3bf06d39ad46ed4c7f1e28071477c9dc4f3
asherswint-sudo <asherswint@billericak12.com>
2025-10-03 10:18:58 -0400

    y

--- Circle.java
+++ Circle.java
@@ -1,15 +1,27 @@
 public class Circle {
     private double radius;
-
+    /**
+     * Constructor for Circle
+     * @param r - radius of the circle
+     */
     public Circle(double r){
         radius = r;
     }
+
+    /**
+     * Calculates the area of the circle
+     * @return the area
+     */
 
     public double area(){
         double a = Math.PI*Math.pow(radius, 2);
         return a;
     }
 
+    /**
+     * Calculates the circumference of the circle
+     * @return the circumference
+     */
     public double circumference(){
         double c = 2*Math.PI*radius;
         return c;
@@ -25,3 +37,6 @@ public class Circle {
     return radius;
     }
 }
+
+// 78.53981633974483
+// 31.41592653589793
--- Cylinder.java
+++ Cylinder.java
@@ -1,17 +1,27 @@
 public class Cylinder {
     private double height;
     private Circle base;
-
+    /**
+     * Constructor for Cylinder
+     * @param r - radius of the base circle
+     * @param h - height of the cylinder
+     */
      public Cylinder(double r, double h){
         base = new Circle(r);
         height = h;
     }
-
+    /**
+     * Calculates the volume of the cylinder
+     * @return the volume
+     */
     public double volume(){
         double v = height * base.area();
         return v;
     }
-
+    /**
+     * Calculates the surface area of the cylinder
+     * @return the surface area
+     */
     public double surfaceArea(){
         double sa = 2*base.area() + height*base.circumference();
         return sa; 
