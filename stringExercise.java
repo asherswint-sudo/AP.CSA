972bd1ecca0f6640d407889d784c4f7f6c8d89c4
asherswint-sudo <asherswint@billericak12.com>
2025-10-15 09:35:04 -0400

    drake maye is the undisputed MVP

--- stringExercise.java
+++ stringExercise.java
@@ -12,19 +12,31 @@ public class stringExercise {
         String rest = word1.substring(1);
         return rest + first; 
     }
-
+    /**
+     * Converts a name from "Last, First" to "First Last"
+     * @param word1 the name in "Last, First" format
+     * @return the name in "First Last" format
+     */
     public static String convertName(String word1){
         int loc1 = word1.indexOf(",");
         String b1 = word1.substring(0, loc1).trim();
         String b2 = word1.substring(loc1 + 1).trim();
         return b2 + " " + b1;
     }
-
+    /**
+     * removes the dashes from a social security number
+     * @param word1 the social security number in the format with dashes
+     * @return the social security number in the format without dashes
+     */
     public static String removeSSN(String word1){
         String c3 = word1.replace("-","");
         return c3;
     }
-
+    /**
+     * Converts a date string from MM/DD/YYYY to DD-MM-YYYY
+     * @param word1 the date string in "MM/DD/YYYY" format
+     * @return the date string in "DD-MM-YYYY" format
+     */
     public static String dateString(String word1){ 
         int loc1 = word1.indexOf("/");
         int loc2 = word1.indexOf("/", loc1+1);
@@ -33,25 +45,41 @@ public class stringExercise {
         String year = word1.substring(loc2 + 1);
         return day + "-" + month + "-" + year;
     }
-    
+    /**
+     * Flips binary digits in a string, changing 0s to 1s and 1s to 0s
+     * @param word1 the binary string to flip
+     * @return the flipped binary string
+     */
     public static String negative(String word1){
         String step1 = word1.replace("0","a");
         String step2 = step1.replace("1","0");
         String step3 = step2.replace("a","1");
         return step3;
     }
-
-    public static String nameGame(String word1){
+    /**
+     * Plays the name game with a given name
+     * @param word1 the name to use in the name game
+     * @return the name game lyrics for the given name
+     */
+    public static void nameGame(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a name.");
+        String word1 = sc.nextLine();
         int loc1 = word1.indexOf(" ");
         String firstName = word1.substring(0, loc1).trim();
         String lastName = word1.substring(loc1).trim();
         String cutWord1 = firstName.substring(1);
-        String line1 = firstName + ", " + firstName + ", bo- B" + cutWord1;
-        String line2 = "Banana-fana fo-F" + cutWord1;
-        String line3 = "Fee-fi-mo-M" + cutWord1;
-        String line4 = firstName.toUpperCase() + "!";
-        String part1 = line1 + " " + line2+ " " + line3 + " " + line4;
-        return part1;
+        String cutWord2 = lastName.substring(1);
+        System.out.println(firstName + ", " + firstName + ", bo- B" + cutWord1);
+        System.out.println("Banana-fana fo-F" + cutWord1);
+        System.out.println("Fee-fi-mo-M" + cutWord1);
+        System.out.println(firstName.toUpperCase() + "! ");
+        System.out.println(" ");
+        System.out.println("Cent, Cent, bo-B" + cutWord2);
+        System.out.println("Banana-fana fo-F" + cutWord2);
+        System.out.println("Fee-fi-mo-M" + cutWord2 + " ");
+        System.out.println(lastName.toUpperCase() + "!");
+        
     }
 
 
@@ -74,10 +102,28 @@ public class stringExercise {
     System.out.println("04/20/2025 becomes " + dateString("04/20/2025"));
     System.out.println("4/2/2025 becomes " + dateString("4/2/2025"));
 
-    
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a word.");
-    String word1 = sc.nextLine();
-    System.out.println(nameGame(word1));
+    nameGame();
    }
 }
+// ello WorldH
+// appyh
+// h
+// Lori Renee Reubenstein
+// Taylor Swift
+// Bozo the Clown
+// 123-45-6789 is 123456789
+// 1101000110
+// 00000000
+// 04/20/2025 becomes 20-04-2025
+// 4/2/2025 becomes 2-4-2025
+// Enter a name.
+// Fifty Cent
+// Fifty, Fifty, bo- Bifty
+// Banana-fana fo-Fifty
+// Fee-fi-mo-Mifty
+// FIFTY!
+
+// Cent, Cent, bo-Bent
+// Banana-fana fo-Fent
+// Fee-fi-mo-Ment
+// CENT!
