e09cf5986f2c3c51f5cef7979eae1ddb04d9eee6
asherswint-sudo <asherswint@billericak12.com>
2025-11-20 10:31:18 -0500

    y

--- U4/WhileExamples.java
+++ U4/WhileExamples.java
@@ -1,27 +1,45 @@
 package U4;
-public class WhileExamples{
-    public static void fibonacci(int max) {
+public class WhileExamples {
+    public static int sumUpToWhile(int n)
+    {
+        int sum = 0;
+        int i = 1;	//initialize
+        while (i <= n)	//test
+        {
+            sum += i;
+            i++;	//increment
+        }
+        return sum;
+    }
+
+    public static void fibbonacci(int max){
         int first = 0;
         int second = 1;
         int counter = first;
+
         System.out.print(first + " ");
         System.out.print(second + " ");
         while (counter < max){
             counter = first + second;
-            System.out.println(counter + " ");
+            System.out.print(counter + " ");
             first = second;
             second = counter;
-
+           
         }
+        System.out.println();
     }
-
     public static void main(String[] args) {
-        
+        int a = 2;
+        while (a <= 7){
+            int b = sumUpToWhile(a);
+            System.out.println(a +" sums " + b);
+            a++;
+        }
 
         int x = 10;
         while(x < 10000){
             fibbonacci(x);
-            x=x*10;
+            x = x*10;
         }
     }
 }
--- U4/WhileLoops.java
+++ U4/WhileLoops.java
@@ -1,5 +1,95 @@
 package U4;
 
 public class WhileLoops {
+    public static int addOdds(int n){
+        int x = 0;
+        int result = 0;
+        while(x < n){ 
+            if (x % 2 != 0) result += x;
+            x += 1; 
+        }
+    if (n % 2 == 0) return result;
+    return result + n;
+    }
+
+    public static int sumDigits(int a){
+        int mod = 10;
+        int div = 1;
+        int b = 0;
+        while (div <= a){
+            b += (a % mod) / div;
+            mod *= 10;
+            div *= 10;
+        }
+        return b;
+    }
     
+    public static int howManyYears(double startpop, double endpop){
+        int years = 0;
+        while(startpop < endpop){
+             startpop *= 1.0113;
+             years ++;
+        }
+        return years;
+    }
+
+    public static void printSum(int n){
+        int a = 0;
+        int result = 0;
+        System.out.println("n = "  + n);
+        while(a < n){
+            result += a;
+            a++;
+            System.out.print(a + " + ");
+            
+        }
+        
+        System.out.print(" = " + result);
+        System.out.println("");
+    }
+    public static boolean isPerfectSquare(int n){
+        int a = 1;
+        int b = 0;
+        while (b < n){
+            b += a;
+            a += 2;
+        }
+        return b == n;
+    }
+    public static void main(String[] args) {
+        System.out.println("addOdds");
+        System.out.println(addOdds(5));
+        System.out.println(addOdds(20));
+        System.out.println(addOdds(11));
+
+        System.out.println("");
+
+        System.out.println("sumDigits");
+        System.out.println(sumDigits(13));
+        System.out.println(sumDigits(600));
+        System.out.println(sumDigits(412));
+
+        System.out.println("");
+
+        System.out.println("howManyYears");
+        System.out.println(howManyYears(111.2, 120));
+        System.out.println(howManyYears(111.2, 150));
+
+        System.out.println("");
+       
+        System.out.println("printSum");
+        printSum(6);
+        printSum(8);
+        printSum(3);
+
+        System.out.println("");
+
+        System.out.println("isPerfectSquare");
+        System.out.println(isPerfectSquare(100));
+        System.out.println(isPerfectSquare(81));
+        System.out.println(isPerfectSquare(33));
+        System.out.println(isPerfectSquare(444));
+
+    }
+
 }
