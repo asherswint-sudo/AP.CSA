4094b77cc85613c0caf0120a039d25772a6e06b0
asherswint-sudo <asherswint@billericak12.com>
2026-02-13 10:25:31 -0500

    y

--- U6/Glossary.java
+++ U6/Glossary.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Glossary {
     static ArrayList<GlossaryEntry> a = new ArrayList<>();
     
-    public int foundOrInserted(String s){
+    public  int foundOrInserted(String s){
     s = s.toUpperCase();
     if (a.size() == 0) {
+        System.out.println(s);
         a.add(new GlossaryEntry(s));
         return 0;
     }
@@ -22,20 +26,43 @@ public class Glossary {
     return 0;
     }
 
+    public void addWord(String word, int line) {
+        int b = foundOrInserted(word);
+        GlossaryEntry entry  = a.get(b);
+        entry.add(line);
+    
 
 
-    public static void main(String[] args) {
-        Glossary test = new Glossary();
-        test.foundOrInserted("hello");
-        test.foundOrInserted("avacado");
-        test.foundOrInserted("krish");
-        test.foundOrInserted("welcome");
-        test.foundOrInserted("stupid");
-        test.foundOrInserted("");
-        int y = 0;
-        for (GlossaryEntry words : a) {
-        System.out.println(words + " " + y);
-        y++;
+    }
+
+    public void addAllWords(String textLine, int lineNumber) {
+        String[] parts = textLine.split(" ");
+        for(int x = 0; x < parts.length; x++){
+            addWord(parts[x], lineNumber);
         }
+    }
+
+    public void printEntries() {
+        System.out.println(a.size());
+        for (int i = 0; i < a.size(); i++) {
+            System.out.println(a.get(i));
+        }
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        File inputfile = new File("fish.txt");
+        Scanner input = new Scanner(inputfile);
+
+        Glossary test = new Glossary();
+        int i = 0;
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            test.addAllWords(line, i);
+            i++;
+        }
+        input.close();
+        test.printEntries();
     }
 }
